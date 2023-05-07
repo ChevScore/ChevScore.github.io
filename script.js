8153666469c0fd2c88c1148aff04f9a1d2b221df
Syed Muhammad Zahir <syed.muhammadzs2002@gmail.com>
2023-05-07 23:33:07 +1000

    Adds main content and menu functionality to the Landing Page

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
+/*===========================================================================*/
+/* Menu                                                                    */
+/*=======================================================================*/
+
+function toggleMenu() {
+    let menuButton = document.getElementById("menu-button");
+    let navLinks = document.getElementById("nav-links");
+    let menuFrame = document.getElementById("menu-frame");
+    let overlay = document.getElementById("overlay");
+
+    menuButton.classList.toggle("open");
+    navLinks.classList.toggle("open");
+    menuFrame.classList.toggle("open");
+    overlay.classList.toggle("open");
+}
+
+createOverlay = () => {
+    let overlay = document.createElement("div");
+    overlay.id = "overlay";
+    document.body.appendChild(overlay);
+}
+
+
+/*===========================================================================*/
+/* Main Frame                                                              */
+/*=======================================================================*/
+
 function drawBackFrame() {
-    let windowWidth = getWindowWidth();
     let windowHeight = getWindowHeight();
+    let windowWidth = getWindowWidth();
 
     // Setting the width and height of the canvas to the width and height of the
     // window
@@ -22,7 +49,7 @@ function drawBackFrame() {
 
     // The bridgeLength is the horizontal length of the dip in the top of the
     // frame
-    let bridgeLength = 0.2 * windowWidth;
+    let bridgeLength = 0.3 * windowWidth;
 
     let ctx = frame.getContext("2d");
     ctx.strokeStyle = "white";
@@ -44,6 +71,55 @@ function drawBackFrame() {
     ctx.stroke();
 }
 
+/*===========================================================================*/
+/* Menu Frame                                                              */
+/*=======================================================================*/
+
+function drawMenuFrame() {
+    let windowHeight = getWindowHeight();
+    let windowWidth = getWindowWidth();
+
+    // Setting the width and height of the canvas to the width and height of the
+    // window
+    frame = document.getElementById("menu-frame");
+    frame.width = windowWidth;
+    frame.height = windowHeight;
+    
+    // The offset is the distance between the edge of the frame and the edge of 
+    // the canvas
+    let offset = 25;
+
+    // The trimming is the vertical and horizontal lengths of the edge 
+    // trimming of the frame
+    let trimming = 40;
+    
+    // The bridgeOffset is the horizontal length from the top right corner of
+    // the frame to the start of the bridge
+    let bridgeOffset = 0.15 * windowWidth + offset;
+
+    // The bridgeLength is the horizontal length of the dip in the top of the
+    // frame
+    let bridgeLength = 0.3 * windowWidth;
+
+    let ctx = frame.getContext("2d");
+    ctx.fillStyle = "#151515";
+
+    // Outlining the back frame through drawing lines
+    ctx.moveTo(windowWidth - bridgeOffset - bridgeLength, windowHeight - offset);
+    ctx.lineTo(windowWidth - offset - trimming, windowHeight - offset);
+    ctx.lineTo(windowWidth - offset, windowHeight - offset - trimming);
+    ctx.lineTo(windowWidth - offset, offset);
+    ctx.lineTo(windowWidth - bridgeOffset, offset);
+    ctx.lineTo(windowWidth - bridgeOffset - trimming, offset + trimming);
+    ctx.lineTo(windowWidth - bridgeOffset - bridgeLength, offset + trimming);
+    ctx.closePath();
+    ctx.fill();
+}
+
+/*===========================================================================*/
+/* General Functions                                                       */
+/*=======================================================================*/
+
 // Returns the width of the window
 function getWindowWidth() {
     return window.innerWidth;
@@ -55,11 +131,18 @@ function getWindowHeight() {
 }
 
 
+/*===========================================================================*/
+/* Event Listeners                                                         */
+/*=======================================================================*/
+
 // Draws the back frame when the window loads and when the window is resized
 window.onload = () => {
     drawBackFrame();
+    drawMenuFrame();
+    createOverlay();
 }
 
 window.onresize = () => {
     drawBackFrame();
+    drawMenuFrame();
 }
